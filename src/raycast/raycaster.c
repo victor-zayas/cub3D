a25fb306bc454f8b8637b217d0e218ea4e5e57f9
lagonzal <larraingonzalez@gmail.com>
2023-11-01 17:46:28 +0100

    West looking in vertical looks okay

--- src/raycast/raycaster.c
+++ src/raycast/raycaster.c
@@ -12,6 +12,10 @@
 
 #include "../../includes/cub3D.h"
 
+#define BL "\033[0;34m"
+#define CY "\033[0;36m"
+#define WT "\033[0;37m"
+
 /**
  * @brief Calculates the first horizontal collisions position and the offset
  * for the next one in case is needed.
@@ -24,7 +28,7 @@ void	h_offset_calc(t_playerpos *p_pos, t_ray *ray)
 {
 	float	arctan;
 
-	arctan = -1 / tan(ray->ra);
+	arctan = -tan(ray->ra);
 	if (ray->ra > PI) //looking down
 	{
 		ray->ry = (((int)p_pos->py >> 6) << 6) - 0.0001;
@@ -59,9 +63,7 @@ void	h_offset_calc(t_playerpos *p_pos, t_ray *ray)
 t_ray	check_h_colision(t_playerpos *p_pos, t_map *map, float ra)
 {
 	t_ray	ray;
-	int		i;
 
-	i = 0;
 	ray.end = 0;
 	ray.ra = ra;
 	ray.end = 0;
@@ -109,20 +111,25 @@ void	v_offset_calc(t_playerpos *p_pos, t_ray *ray)
 {
 	float	ntan;
 
-	ntan = -tan(ray->ra);
+	ntan = tan(ray->ra);
+	printf("pi rads ra: %f\n", ray->ra / PI);
+	printf("ntan: %f\n", ntan);
 	if (ray->ra > P2 && ray->ra < P32) //looking left
 	{
 		ray->rx = (((int)p_pos->px >> 6) << 6) - 0.0001;
 		ray->ry = (p_pos->px - ray->rx) * ntan + p_pos->py;
+		printf("rx: %d ry: %d\n", (int)ray->rx>>6, (int)ray->ry>>6);
+		printf("px - rx: %f, * ntan: %f, + py: %f\n", p_pos->px - ray->rx, (p_pos->px - ray->rx) * ntan, (p_pos->px - ray->rx) * ntan + p_pos->py);
 		ray->xo = -64;
-		ray->yo = -ray->xo * ntan;
+		ray->yo = ray->xo * -ntan;
+		printf("xo: %f yo: %f\n", ray->xo, ray->yo);
 	}
 	if (ray->ra < P2 || ray->ra > P32) //looking right
 	{
 		ray->rx = (((int)p_pos->px >> 6) << 6) +64;
 		ray->ry = (p_pos->px - ray->rx) * ntan + p_pos->py;
 		ray->xo = 64;
-		ray->yo = -ray->xo * ntan;
+		ray->yo = ray->xo * -ntan;
 	}
 	if (ray->ra == P2 || ray->ra == P32) //looking straight up or straight down
 	{
@@ -144,11 +151,9 @@ void	v_offset_calc(t_playerpos *p_pos, t_ray *ray)
 t_ray	check_v_colision(t_playerpos *p_pos, t_map *map, float ra)
 {
 	t_ray	ray;
-	int		i;
 
 	ray.ra = ra;
 	ray.end = 0;
-	i = 0;
 	v_offset_calc(p_pos, &ray);
 	while (!ray.end)
 	{
@@ -156,7 +161,7 @@ t_ray	check_v_colision(t_playerpos *p_pos, t_map *map, float ra)
 		//printf("player position x:%d y:%d\n", (int)(p_pos->px) >> 6, (int)(p_pos->py) >> 6);
 		ray.my = (int) (ray.ry) >> 6;
 		ray.mx = (int) (ray.rx) >> 6;
-		printf("vertical========================\nray rx %f mx:%d ray ry %f my: %d\n",ray.rx >> 6, ray.mx,ray.ry >> 6, ray.my);
+		printf("vertical========================\nray rx %f mx:%d ray ry %f my: %d\n",ray.rx, ray.mx,ray.ry, ray.my);
 		if (map->height <= ray.my || (int)ft_strlen(map->map[ray.my]) <= ray.mx || ray.mx < 0 || ray.my < 0)
 		{
 //			printf("first condition: %d second condition %d\n", map->height < ray.ry, map->width < ray.rx);
@@ -181,6 +186,38 @@ t_ray	check_v_colision(t_playerpos *p_pos, t_map *map, float ra)
 		return (ray.tx = 'E', ray);
 }
 
+
+void	print_map(t_map *map)
+{
+	int	i;
+	int	j;
+	int	k;
+
+	i = 0;
+	printf("   ");
+	while (i < map->width)
+		printf("%s|%s%03d",CY, BL, i++);
+	printf("%s|\n", CY);
+	i = 0;
+	while (map->map[i])
+	{
+		k = 0;
+		while (k < map->width * 3)
+		{
+			printf("%s__", CY);
+			k++;
+		}
+		printf("\n");
+		printf("%s%03d", BL, i);
+		j = 0;
+		while (map->map[i][j])
+		{
+			printf("%s|%s %c ", CY, WT, map->map[i][j++]);
+		}
+		printf("%s|\n%s", CY, WT);
+		i++;
+	}
+}
 /**
  * @brief casts the  rays and decide the texture that is needed for the column.
  * 
@@ -189,15 +226,18 @@ t_ray	check_v_colision(t_playerpos *p_pos, t_map *map, float ra)
  * @param text	texture
  */
 
-void	raycaster(t_playerpos *p_pos, t_all *all, t_mlx *mlx) //here we need to get the big structure as we will need it later to draw the line.
+void	raycaster(t_playerpos *p_pos, t_all *all/*, t_mlx *mlx*/) //here we need to get the big structure as we will need it later to draw the line.
 {
 	t_ray	vc;
 	t_ray	hc;
 	float	ra;
 	int		i;
+	char	c;
 
 	ra = p_pos->pa + (46 * DEG);
 	i = 0;
+	printf("playerpos: px = %f py = %f pa = %f\n", p_pos->px, p_pos->py, p_pos->pa);
+	printf("ra in rads: %f\n", ra / PI);
 	while (i < 90)
 	{
 		ra = fix_angle(ra - DEG);
@@ -210,7 +250,7 @@ void	raycaster(t_playerpos *p_pos, t_all *all, t_mlx *mlx) //here we need to get
 			//take_texture(&hc, text);
 			// printf("ray.mx: %d ray.my: %d\n", hc.mx, hc.my);
 			// printf("dist: %f\n", hc.dist);
-			draw_column(&hc, all, mlx, i);
+			//draw_column(&hc, all, mlx, i);
 		}
 		else
 		{
@@ -219,10 +259,12 @@ void	raycaster(t_playerpos *p_pos, t_all *all, t_mlx *mlx) //here we need to get
 			//printf("vertical\n");
 			// printf("dist: %f\n", vc.dist);
 			// printf("ray.mx: %d ray.my: %d\n", hc.mx, hc.my);
-			draw_column(&vc, all, mlx, i);
+			//draw_column(&vc, all, mlx, i);
 		}
+		print_map(&all->map);
+		scanf("%c\n", &c);
 		printf("%d\n", i);
 		i++;
 	}
-	mlx_put_image_to_window(mlx->mlx, mlx->win, mlx->img, 0, 0);
+	//mlx_put_image_to_window(mlx->mlx, mlx->win, mlx->img, 0, 0);
 }
